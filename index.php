072f73802c4ffa5dee0d398e0854a70674a4bd59
anatoliist <anatolii.st@gmail.com>
2014-07-21 10:40:42 +0400

    Мой первый коммит

---
+++ index.php
@@ -0,0 +1,138 @@
+<!doctype html>
+<html>
+	<head>
+		<meta charset="UTF-8" />
+		<link rel="stylesheet" href="style.css" media="all" />
+		<script type="text/javascript" src="jquery.min.js"></script>
+		<script src="/js.js"></script>
+		<title>Сайт об авто</title>
+	</head>
+	<body>
+		<div class="header">
+			<div class="inside">
+				<div class="top_menu">
+					<div class="packaging">
+						<ul class="menu_list">
+							<li class="first"><a href="#">Главная</a></li>
+							<li><a href="#">Новости</a></li>
+							<li><a href="#">Статьи</a></li>
+							<li><a href="#">О Нас</a></li>
+							<li><a href="#">Контакты</a></li>
+						</ul>
+					</div>
+				</div>
+				<div class="about">
+					<h4>Профиль Водителя:</h4>
+					<img src="i/header_about_img.png" alt="Мое фото" class="im"><br>
+					<div>
+						<span class="desc">- Имя: &nbsp;&nbsp;&nbsp;Andrew Romanov</span><br>
+						<span class="desc">- Сайт: &nbsp;&nbsp;www.dieflix.ru</span>
+					</div>
+				</div>
+				<div class="acii_list">					
+					<div>
+						<a class="rhead" href="#">Заказать Рекламу</a>
+						<a href="#" class="mesto"><img src="i/header_aciilist_img1.jpg" alt="Здесь может быть ваша реклама."></a>
+						<a href="#" class="mesto"><img src="i/header_aciilist_img1.jpg" alt="Здесь может быть ваша реклама."></a>
+						<a href="#" class="mesto"><img src="i/header_aciilist_img1.jpg" alt="Здесь может быть ваша реклама."></a>
+					</div>
+					<img class="background_" src="i/header_aciilist_background.png" alt="">
+					
+				</div>
+				<div class="logo"><img src="i/header_logo_background.png" alt="Логотип"></div>
+			</div>
+		</div>
+		<div class="clear"></div>
+		<div class="container">
+			<div class="inside">
+				<div class="content">
+					<img src="i/content_img1.png" alt="Превью авто" class="head">
+					<h2>Главные новости автомира:</h2>
+					<div class="post">
+						<a href="#" class="name">Жара: водителей просят отказаться от автомобилей</a>
+						<div class="img_shadow"><img src="i/content_post_img1.jpg" alt="Фото 1" class="prev"></div>
+						<p>ГИБДД Москвы рекомендует водителям быть максимальной осторожным во время жаркой погоды, передает «РИА Новости». Мало того, сотрудники автоинспекции рекомендуют вообще не ездить в дневное время, особенно если в автомобиле нет кондиционера. </p>
+						<span class="info">Дата: 23/06/2013  |  Просмотров: 2321  |  Комментарии: 13 </span>
+						<a href="#" class="here"></a>
+					</div>
+					<div class="post">
+						<a href="#" class="name">Оформлением мелких ДТП займутся страховщики</a>
+						<div class="img_shadow"><img src="i/content_post_img2.jpg" alt="Фото 2" class="prev"></div>
+						<p>К 2016 году оформление мелких ДТП без пострадавших планируется передать страховщикам. Об этом говорится в плане деятельности Министерства внутренних дел России на период с 2013 по 2018 годы. </p>
+						<span class="info">Дата: 23/06/2013  |  Просмотров: 2321  |  Комментарии: 13 </span>
+						<a href="#" class="here"></a>
+					</div>
+					<div class="post">
+						<a href="#" class="name">В правила добавлены знак и разметка «Фотовидеофиксация»</a>
+						<div class="img_shadow"><img src="i/content_post_img3.jpg" alt="Фото 3" class="prev"></div>
+						<p>И стория со знаком, предупреждающим водителей о фотовидеофиксации нарушений, завершилась принятием изменений в правилах дорожного движения. С 1 июля нынешнего года они вступают в силу. Помните знак, за который проголосовала большая часть посетителей сайта ГИБДД? Он послужил основой для нового знака дополнительной информации (то есть таблички) 8.23, названной «Фотовидеофиксация». </p>
+						<span class="info">Дата: 23/06/2013  |  Просмотров: 2321  |  Комментарии: 13 </span>
+						<a href="#" class="here"></a>
+					</div>
+				<div class="social"><img src="i/content_img5.jpg" alt="Социальные сети"></div>
+				</div>	
+				<div class="siderbar">
+					<div class="cat">
+						<img src="i/siderbar_img1.png" alt="Категории фото" class="prev">
+						<div class="date">
+							<h3>Категории:</h3>
+							<ul class="list">							
+								<li>- <a href="#">О компании</a></li>
+								<li>- <a href="#">Автомобили и цены</a></li>
+								<li>- <a href="#">Ремонт и т.о.</a></li>
+								<li>- <a href="#">Настройка</a></li>
+								<li>- <a href="#">Тюнинг авто.</a></li>
+								<li>- <a href="#">Пдд и штрафы.</a></li>
+								<li>- <a href="#">Автошколы</a></li>
+							</ul>
+						</div>
+					</div>
+					<div class="cat">
+						<img src="i/siderbar_img2.png" alt="Соревнования" class="prev">
+						<div class="date">
+							<h3>Соревнования:</h3>
+							<ul class="list">
+								<li>- <a href="#">Чемпионаты</a></li>
+								<li>- <a href="#">Дневные заезды</a></li>
+								<li>- <a href="#">Ночные заезды</a></li>
+								<li>- <a href="#">Звёзды дрифты</a></li>
+								<li>- <a href="#">Настройка АВТО</a></li>
+								<li>- <a href="#">Призы</a></li>
+							</ul>
+						</div>
+					</div>
+					<div class="cat">
+						<img src="i/siderbar_img3.png" alt="Лучший водитель" class="prev">
+						<div class="date">
+							<h3>Лучший водитель:</h3>
+							<a href="#" class="video_top"><img src="i/siderbar_img4.jpg" alt="Видео лучший водитель"></a><br>
+							<a href="#" class="video_link">Alexandr Konevskiu (Maybe Lie)</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>	
+		<div class="footer">
+			<div class="inside">
+				<div class="follow">
+					<h4>Follow us</h4>
+					<ul class="follow_list">
+						<li><a href="#"><img src="i/footer_socials_img1.gif" alt="Facebook"><span>Facebook</span></a></li>
+						<li><a href="#"><img src="i/footer_socials_img2.gif" alt="Subscribe"><span>Subscribe</span></a></li>
+						<li><a href="#"><img src="i/footer_socials_img3.gif" alt="Twitter"><span>Twitter</span></a></li>
+						<li><a href="#"><img src="i/footer_socials_img4.gif" alt="Flickr"><span>Flickr </span></a></li>
+					</ul>
+				</div>
+				<div class="contact">
+					<h4>Контакты: </h4>
+					<p>Телефоны: (495) 432-32-32 (925)<br>322-12-12<br>Почта: AutoSport@auto.ru<br>Для справок: www.AUTOst.ru<br> (c) 2001 - 2013 ct.&nbsp;&nbsp;</p>
+				</div>
+				<div class="stat">
+					<h4>Статистика:</h4>
+					<img src="i/footer_stat_img.gif" alt="Фото статистика">
+				</div>
+				<a href="#" class="copy"></a>
+			</div>
+		</div>
+	</body>
+</html>
